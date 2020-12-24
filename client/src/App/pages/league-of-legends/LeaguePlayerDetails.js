2962a36f5d78791f3dae1a5d1797868d5aa0056a
Lennart Michalke <l.michalke@tu-bs.de>
2020-12-24 22:52:54 +0100

    added matchhistory infos

--- client/src/App/pages/league-of-legends/LeaguePlayerDetails.js
+++ client/src/App/pages/league-of-legends/LeaguePlayerDetails.js
@@ -7,8 +7,39 @@ import {getRank} from './LeagueOfLegendsApi.js';
 function LeaguePlayerDetails(props) {
   const [player, setPlayer] = useState({});
 
+  //declare version and set it to 10.25.1 as a fallback
+  let version = "10.25.1";
+
+  const getDDragonCurrentVersion = async () => {
+    const rawJSONVersion = await fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(res => res.json());
+    return rawJSONVersion[0];
+  }
+  
+  //inefficient => maybe create a file that saves each champion id and their corresponding name and use to write into the file if the champion isnt in there => fallback for new champions 
+  const getChampionName = async (id) => {
+    const rawJSONDDragon = await fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/data/de_DE/champion.json`).then(res => res.json());
+    let championList = rawJSONDDragon.data;
+    for (var i in championList) {
+      if (championList[i].key === String(id)) {
+        return championList[i].id;
+      }
+    }
+  }
+  
+  const getSummonerSpellName = async (id) => {
+    const rawJSONDDragon = await fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/summoner.json`).then(res => res.json());
+    let summonerSpellList = rawJSONDDragon.data;
+    for (var i in summonerSpellList) {
+      if (summonerSpellList[i].key === String(id)) {
+        return summonerSpellList[i].id;
+      }
+    }
+  }
+
   useEffect(() => {
     const fetchPlayer = async () => {
+      //get newest version
+      version = await getDDragonCurrentVersion();
       const fetchedPlayer = await fetch("http://localhost:3001/api/league-of-legends/players/" + props.match.params.player).then(res => res.json());
       var player = {};
       player.leagueName = fetchedPlayer.summonerInfo.name;
@@ -26,7 +57,28 @@ function LeaguePlayerDetails(props) {
         const rawJsonMatch = await fetch("http://localhost:3001/api/league-of-legends/match/" + matchList[i].gameId).then(res => res.json());
         let match = {};
         match.gameId = rawJsonMatch.matchInfo.gameId;
-        //loop through all pariticpants, check if current participant == player
+        
+        //create participant lists
+        var participantListPlayerTeam = {"data": []};
+        var participantListEnemyTeam = {"data": []};
+        for (var k in rawJsonMatch.matchInfo.participants) {
+          var participant = {};
+          participant.stats = rawJsonMatch.matchInfo.participants[k];
+          participant.identity = rawJsonMatch.matchInfo.participantIdentities.find(item => item.participantId === participant.stats.participantId);
+          if (participant.stats.teamId === 100) {
+            participantListPlayerTeam.data.push(participant);
+          } else {
+            participantListEnemyTeam.data.push(participant);
+          }
+          participant.championName = await getChampionName(participant.stats.championId);
+          participant.championSrc = `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${participant.championName}.png`;
+        }
+        match.participantListPlayerTeam = participantListPlayerTeam;
+        match.participantListEnemyTeam = participantListEnemyTeam;
+
+        console.log(match);
+
+        //loop through all pariticpants, check if current participant === player
         for (var j = 0; j < rawJsonMatch.matchInfo.participantIdentities.length; j++) {
           const participant = rawJsonMatch.matchInfo.participantIdentities[j];
           if (participant.player.summonerName === player.leagueName) {
@@ -39,6 +91,15 @@ function LeaguePlayerDetails(props) {
             match.kills = rawJsonMatch.matchInfo.participants[j].stats.kills;
             match.deaths = rawJsonMatch.matchInfo.participants[j].stats.deaths;
             match.assists = rawJsonMatch.matchInfo.participants[j].stats.assists;
+
+            match.championPlayedByName = await getChampionName(rawJsonMatch.matchInfo.participants[j].championId);
+            match.championPlayedSrc = `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${match.championPlayedByName}.png`;
+
+            match.summonerSpell1ByName = await getSummonerSpellName(rawJsonMatch.matchInfo.participants[j].spell1Id);
+            match.summonerSpell2ByName = await getSummonerSpellName(rawJsonMatch.matchInfo.participants[j].spell2Id);
+            match.summonerSpell1Src = `https://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${match.summonerSpell1ByName}.png`;
+            match.summonerSpell2Src = `https://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${match.summonerSpell2ByName}.png`;
+
             break;
           }
         }
@@ -82,6 +143,41 @@ function LeaguePlayerDetails(props) {
                 <div className="match-container" key={`${match.gameId}match-container`}>
                   <div className="win-loose-indicator-container" key={`${match.gameId}win-loose-indicator-container`}>
                     <div className="win-loose-indicator" style={{backgroundColor: match.winColor}} key={`${match.gameId}win-loose-indicator`} />
+                  </div>
+                  <div className="champion-summonerspells-container" key={`${match.gameId}champion-summonerspells-container`}>
+                    <div className="own-champion-container" key={`${match.gameId}own-champion-container`}>
+                      <img className="own-champion" src={match.championPlayedSrc} alt="" />
+                    </div>
+                    <div className="summonerspells-container" key={`${match.gameId}summonerspells-container`}>
+                      <img className="summonerspell" src={match.summonerSpell1Src} alt="" />
+                      <img className="summonerspell" src={match.summonerSpell2Src} alt="" />
+                    </div>
+                  </div>
+                  <div className="kda-container">
+                    <h1 className="kda">{match.kills}:{match.deaths}:{match.assists}</h1>
+                  </div>
+                  <div className="participantList-full-container" key={`${match.gameId}participantList-container`}>
+                    <div className="participantList-container" key={`${match.gameId}participantList-PlayerTeam-container`}>
+                      {
+                        match.participantListPlayerTeam.data && match.participantListPlayerTeam.data.map(participant =>
+                          <div className="participant-container" key={`${match.gameId}${participant.identity.player.summonerName}participant-containerPlayerTeam`}>
+                            <img className="participantChampion" src={participant.championSrc} alt="" />
+                            <h1 className="participantName">{participant.identity.player.summonerName}</h1>
+                          </div>
+                        )
+                      }
+                    </div>
+                    <div className="participantList-container" key={`${match.gameId}participantList-EnemyTeam-container`}>
+                      {
+                        match.participantListEnemyTeam.data && match.participantListEnemyTeam.data.map(participant =>
+                          <div className="participant-container" key={`${match.gameId}${participant.identity.player.summonerName}participant-containerEnemyTeam`}>
+                            <img className="participantChampion" src={participant.championSrc} alt="" />
+                            <h1 className="participantName">{participant.identity.player.summonerName}</h1>
+                          </div>
+                        )
+                      }
+                    </div>
+
                   </div>
                 </div>
               )
